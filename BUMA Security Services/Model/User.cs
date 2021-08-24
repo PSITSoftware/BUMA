d14474d1118d51efc86ad3397185c1b50e15930d
Silvio Alejandro Reyes Jota <info@psit.com.co>
2021-08-23 19:33:55 -0500

    Validacion repositorio

---
+++ BUMA Security Services/Model/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BUMA.Security.Services.Model
+{
+    public class User
+    {
+    }
+}
