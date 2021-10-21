87d9de271a5d24e630a4c36c4073672dcbf9ac74
Silvio Alejandro Reyes Jota <info@psit.com.co>
2021-10-20 20:14:47 -0500

    Se generan los proyectos correspondientes servicios, interfaces e implementacion

---
+++ BUMA Security Services/Controller/UsuarioController.cs
@@ -0,0 +1,112 @@
+﻿// ==================================================================================
+// Desarrollado Por	    : Silvio Alejandro Reyes Jota
+// Fecha de Creación	: 20/10/2021
+// Producto o sistema	: PIDT
+// Empresa	: Tecna ICE S.A
+// Proyecto	: PIDT 6
+// Cliente	: todos
+// ==================================================================================
+// Versión	Descripción
+// 1.0.0.0	<Descripción del archivo>
+//
+// ==================================================================================
+// HISTORIAL DE CAMBIOS:
+// =================================================================================
+// Ver.	Fecha	        Autor	                Descripción
+// ---	-------------	----------------------	-----------------------------------
+// 0.1	20/10/2021	    Silvio Alejandro Reyes Jota	    Desarrollo Inicial
+
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Security.Policy;
+using System.Data;
+using buma.dominio.DTO.Seguridad;
+using buma.security.servicios.interfaces;
+
+namespace psit.buma.security.services.Controllers
+{ 
+  
+
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioServicio _usuarioServicio;
+        public UsuarioController(IConfiguration configuration, IUsuarioServicio usuarioServicio)
+        {
+            _usuarioServicio = usuarioServicio;
+        }
+
+
+        /// <summary>
+        /// Obtiene el listado de todos los Usuarios
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<List<Usuario>> GetUsuariosListAsync()
+        {
+
+            return await _usuarioServicio.GetUsuariosListAsync();
+        }
+
+        /// <summary>
+        /// Obtiene los Usuarios por Identificador
+        /// </summary>
+        /// <param name="idUsuario"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<Usuario> GetUsuarioByIdAsync(int idUsuario)
+        {
+            var usuario = new Usuario();
+            return usuario;
+        }
+       
+        /// <summary>
+        /// Inserta la informaciòn del usuario
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// <returns></returns>
+
+        [HttpPost]
+        public async Task InsertUsuario(Usuario usuario)
+        {
+            try
+            {
+              
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        /// <summary>
+        /// Actualiza la informaciòn del usuario
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task UpdateUsuario(Usuario usuario)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
--- Buma.Dominio/DTO/Seguridad/Usuario.cs
+++ Buma.Dominio/DTO/Seguridad/Usuario.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Buma.Dominio.DTO.Seguridad
+namespace buma.dominio.DTO.Seguridad
 {
     public class Usuario
     {
