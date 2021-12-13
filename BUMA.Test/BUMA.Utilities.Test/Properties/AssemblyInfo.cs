b6941b0a1dbbaead6452a1dbc3f1d76d3c8e9ded
Silvio Alejandro Reyes Jota <info@psit.com.co>
2021-12-12 21:20:36 -0500

    se genera proceso de conexion a base de datos postgress

---
+++ BUMA.Test/BUMA.Utilities.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BUMA.Utilities.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BUMA.Utilities.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("483303e4-d6cb-4b4d-ae96-ad60a575c0f1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
