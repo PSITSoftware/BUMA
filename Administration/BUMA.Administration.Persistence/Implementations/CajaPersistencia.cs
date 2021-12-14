9b282ecd0e9910a5eb8050c36de7bd918b0cc67d
Silvio Alejandro Reyes Jota <info@psit.com.co>
2021-12-13 21:43:59 -0500

    validacion de llamado store procedue

--- Administration/BUMA.Administration.Persistence/Implementations/CajaPersistencia.cs
+++ Administration/BUMA.Administration.Persistence/Implementations/CajaPersistencia.cs
@@ -19,22 +19,79 @@ using BUMA.Administration.Persistence.Interfaces;
 using BUMA.Domain.DTO.Administration;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using BUMA.Utilities;
+using BUMA.Utilities.Connection;
+using Npgsql;
+using System.Data;
+using System;
+using System.Linq;
 
 namespace BUMA.Administration.Persistence.Implementations
 {
     public class CajaPersistencia : ICajaPersistencia
     {
+        PostgressConection _idConnection;
+        public async Task npglsqlConection()
+        {
+            PostgressConection npgcon = new PostgressConection();
+            npgcon.ConnectionPostgressAdmin().Open();
+            _idConnection = npgcon;
 
+        }
         public async Task<List<Caja>> GetCajaListAsync()
         {
-            var caja = new List<Caja>();
-            return caja;
+            try
+            {
+                // Start a transaction as it is required to work with cursors in PostgreSQL
+                NpgsqlCommand command = new NpgsqlCommand("SelectCaja", _idConnection.ConnectionPostgressAdmin());
+                command.CommandType = CommandType.StoredProcedure;
+
+
+                // Execute the stored procedure and obtain the first result set
+                NpgsqlDataReader dr = command.ExecuteReader();
+                //var caja = new List<Caja>();
+                //return caja;
+            }
+            catch (NpgsqlException ex)
+            {
+
+            }
+            return new List<Caja>();
+
         }
         public async Task<Caja> GetCajaByIdAsync(int idCaja)
         {
-            var caja = new Caja();
-            return caja;
+            var _caja = new List<Caja>();
+            //var caja = new Caja();
+            //return caja;
+            try
+            {
+                // Start a transaction as it is required to work with cursors in PostgreSQL
+                NpgsqlCommand command = new NpgsqlCommand("SelectIdCaja", _idConnection.ConnectionPostgressAdmin());
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@pid_caja", idCaja);
+
+
+                // Execute the stored procedure and obtain the first result set
+                NpgsqlDataReader dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    var cajas = new Caja()
+                    {
+                        idcaja = Convert.ToInt32(dr["Id_Caja"]),
+                        nombrecaja = Convert.ToString(dr["Nombre"]),
+                        codigocaja = Convert.ToString(dr["Codigo_Caja"]),
+                        esprincipal = Convert.ToInt32(dr["Es_Principal"])
+                    };
+                    _caja.Add(cajas);
+                }
+              
+            }
+            catch (NpgsqlException ex)
+            {
+
+            }
+            /// validar el retorno
+            return null;
         }
         public async Task InsertCaja(Caja caja)
         {
